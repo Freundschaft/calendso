4762893e9ca2a5162d205d369d019deeadbed0ac
Qiong Wu <qiong.wu@gfnork.de>
2021-06-14 23:34:13 +0300

    add weekday checkboxes

--- pages/availability/event/[type].tsx
+++ pages/availability/event/[type].tsx
@@ -275,6 +275,140 @@ export default function EventType(props) {
                                                    placeholder="15" defaultValue={props.eventType.maxAttendees}/>
                                         </div>
                                     </div>
+                                    <span className="font-medium text-gray-700">Available Days</span>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-1"
+                                                    name="weekday-1"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-1" className="font-medium text-gray-700">
+                                                    Monday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-2"
+                                                    name="weekday-2"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-2" className="font-medium text-gray-700">
+                                                    Tuesday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-3"
+                                                    name="weekday-3"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-3" className="font-medium text-gray-700">
+                                                    Wednesday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-4"
+                                                    name="weekday-4"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-4" className="font-medium text-gray-700">
+                                                    Thursday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-5"
+                                                    name="weekday-5"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-5" className="font-medium text-gray-700">
+                                                    Friday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-6"
+                                                    name="weekday-6"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-6" className="font-medium text-gray-700">
+                                                    Saturday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="my-8">
+                                        <div className="relative flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    ref={isHiddenRef}
+                                                    id="weekday-0"
+                                                    name="weekday-0"
+                                                    type="checkbox"
+                                                    className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                                                    defaultChecked={props.eventType.hidden}
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="weekday-0" className="font-medium text-gray-700">
+                                                    Sunday
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div className="my-8">
                                         <div className="relative flex items-start">
                                             <div className="flex items-center h-5">
