aa0ffc13d07ac35f19428eada7c73b746ff8a7ce
Qiong Wu <qiong.wu@gfnork.de>
2021-06-15 02:00:12 +0300

    reformat

--- next.config.js
+++ next.config.js
@@ -1,36 +1,32 @@
-
 const withTM = require('next-transpile-modules')(['react-timezone-select']);
 
 // TODO: Revisit this later with getStaticProps in App
 if (process.env.NEXTAUTH_URL) {
-    process.env.BASE_URL = process.env.NEXTAUTH_URL.replace('/api/auth', '');
+  process.env.BASE_URL = process.env.NEXTAUTH_URL.replace('/api/auth', '');
 }
 
-if ( ! process.env.EMAIL_FROM ) {
-    console.warn('\x1b[33mwarn', '\x1b[0m', 'EMAIL_FROM environment variable is not set, this may indicate mailing is currently disabled. Please refer to the .env.example file.');
+if (!process.env.EMAIL_FROM) {
+  console.warn('\x1b[33mwarn', '\x1b[0m', 'EMAIL_FROM environment variable is not set, this may indicate mailing is currently disabled. Please refer to the .env.example file.');
 }
 if (process.env.BASE_URL) {
-    process.env.NEXTAUTH_URL = process.env.BASE_URL + '/api/auth';
+  process.env.NEXTAUTH_URL = process.env.BASE_URL + '/api/auth';
 }
 
 const validJson = (jsonString) => {
-    try {
-        const o = JSON.parse(jsonString);
-        if (o && typeof o === "object") {
-            return o;
-        }
+  try {
+    const o = JSON.parse(jsonString);
+    if (o && typeof o === "object") {
+      return o;
     }
-    catch (e) {}
-    return false;
+  }
+  catch (e) {
+  }
+  return false;
 }
 
-if (process.env.GOOGLE_API_CREDENTIALS && ! validJson(process.env.GOOGLE_API_CREDENTIALS)) {
-    console.warn('\x1b[33mwarn', '\x1b[0m', "- Disabled 'Google Calendar' integration. Reason: Invalid value for GOOGLE_API_CREDENTIALS environment variable. When set, this value needs to contain valid JSON like {\"web\":{\"client_id\":\"<clid>\",\"client_secret\":\"<secret>\",\"redirect_uris\":[\"<yourhost>/api/integrations/googlecalendar/callback>\"]}. You can download this JSON from your OAuth Client @ https://console.cloud.google.com/apis/credentials.");
+if (process.env.GOOGLE_API_CREDENTIALS && !validJson(process.env.GOOGLE_API_CREDENTIALS)) {
+  console.warn('\x1b[33mwarn', '\x1b[0m', "- Disabled 'Google Calendar' integration. Reason: Invalid value for GOOGLE_API_CREDENTIALS environment variable. When set, this value needs to contain valid JSON like {\"web\":{\"client_id\":\"<clid>\",\"client_secret\":\"<secret>\",\"redirect_uris\":[\"<yourhost>/api/integrations/googlecalendar/callback>\"]}. You can download this JSON from your OAuth Client @ https://console.cloud.google.com/apis/credentials.");
 }
-
-console.log(process.env.BASE_URL);
-console.log(process.env.DATABASE_URL);
-console.log(process.env.NEXTAUTH_URL);
 
 module.exports = withTM({
   future: {
