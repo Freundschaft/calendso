cb0f84df81b0d8e4a21ae3149aa87688f8617590
Qiong Wu <qiong.wu@gfnork.de>
2021-06-15 01:54:40 +0300

    test process env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 
 const withTM = require('next-transpile-modules')(['react-timezone-select']);
 
-console.log(process.env.BASE_URL);
-console.log(process.env.DATABASE_URL);
-
 // TODO: Revisit this later with getStaticProps in App
 if (process.env.NEXTAUTH_URL) {
     process.env.BASE_URL = process.env.NEXTAUTH_URL.replace('/api/auth', '');
@@ -30,6 +27,10 @@ const validJson = (jsonString) => {
 if (process.env.GOOGLE_API_CREDENTIALS && ! validJson(process.env.GOOGLE_API_CREDENTIALS)) {
     console.warn('\x1b[33mwarn', '\x1b[0m', "- Disabled 'Google Calendar' integration. Reason: Invalid value for GOOGLE_API_CREDENTIALS environment variable. When set, this value needs to contain valid JSON like {\"web\":{\"client_id\":\"<clid>\",\"client_secret\":\"<secret>\",\"redirect_uris\":[\"<yourhost>/api/integrations/googlecalendar/callback>\"]}. You can download this JSON from your OAuth Client @ https://console.cloud.google.com/apis/credentials.");
 }
+
+console.log(process.env.BASE_URL);
+console.log(process.env.DATABASE_URL);
+console.log(process.env.NEXTAUTH_URL);
 
 module.exports = withTM({
   future: {
